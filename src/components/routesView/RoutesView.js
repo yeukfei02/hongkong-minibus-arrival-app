1b809ebbd47dd58540eccbb6a1fc8ac0839a7577
Donald Wu <yeukfei02@gmail.com>
2022-09-29 10:41:54 +0800

    feat: 🎸 add RouteStopArrival

--- src/components/routesView/RoutesView.js
+++ src/components/routesView/RoutesView.js
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 import Routes from "../routes/Routes";
 import BusRoute from "../busRoute/BusRoute";
 import RouteStop from "../routeStop/RouteStop";
+import RouteStopArrival from "../routeStopArrival/RouteStopArrival";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ function RoutesView() {
       <Stack.Screen name={t("routes")} component={Routes} />
       <Stack.Screen name={t("busRoute")} component={BusRoute} />
       <Stack.Screen name={t("routeStop")} component={RouteStop} />
+      <Stack.Screen name={t("routeStopArrival")} component={RouteStopArrival} />
     </Stack.Navigator>
   );
 }
