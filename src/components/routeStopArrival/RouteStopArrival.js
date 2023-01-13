287954478fca001f57956d5f270331b76276a45d
Donald Wu <yeukfei02@gmail.com>
2023-01-13 20:18:05 +0800

    feat: 🎸 add loading and no data ui

--- src/components/routeStopArrival/RouteStopArrival.js
+++ src/components/routeStopArrival/RouteStopArrival.js
@@ -4,6 +4,7 @@ import { Card, Title } from "react-native-paper";
 import { useRoute } from "@react-navigation/native";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
+import _ from "lodash";
 import moment from "moment";
 import "moment-timezone";
 import { getRootUrl } from "../../helper/helper";
@@ -27,6 +28,7 @@ function RouteStopArrival() {
   const route = useRoute();
   const { t } = useTranslation();
 
+  const [loading, setLoading] = useState(true);
   const [routeStopArrival, setRouteStopArrival] = useState([]);
 
   useEffect(() => {
@@ -51,51 +53,70 @@ function RouteStopArrival() {
       console.log("responseData = ", responseData);
 
       if (responseData) {
+        setLoading(false);
         setRouteStopArrival(responseData.routeStopArrival[0].eta);
       }
     }
   };
 
   const renderRouteStopArrival = () => {
-    let routeStopArrivalView = null;
+    let routeStopArrivalView = (
+      <Card style={styles.cardContainer}>
+        <Card.Content style={{ alignSelf: "center" }}>
+          <Title>{t("pleaseWait")}</Title>
+        </Card.Content>
+      </Card>
+    );
 
-    if (routeStopArrival) {
-      routeStopArrivalView = routeStopArrival.map((item, i) => {
-        return (
-          <View key={i}>
-            <Card style={styles.cardContainer}>
-              <Card.Title title={`${t("next")} ${item.eta_seq} ${t("bus")}`} />
-              <Card.Content>
-                <Title>
-                  {t("remainingTime")} {getMinutesDiffStr(item.timestamp)}
-                </Title>
-              </Card.Content>
-            </Card>
-          </View>
+    if (!loading) {
+      if (!_.isEmpty(routeStopArrival)) {
+        routeStopArrivalView = routeStopArrival.map((item, i) => {
+          return (
+            <View key={i}>
+              <Card style={styles.cardContainer}>
+                <Card.Title
+                  title={`${t("next")} ${item.eta_seq} ${t("bus")}`}
+                />
+                <Card.Content>
+                  <Title>
+                    {t("remainingTime")} {getMinutesDiffStr(item.timestamp)}
+                  </Title>
+                </Card.Content>
+              </Card>
+            </View>
+          );
+        });
+      } else {
+        routeStopArrivalView = (
+          <Card style={styles.cardContainer}>
+            <Card.Content style={{ alignSelf: "center" }}>
+              <Title style={{ color: "red" }}>{t("noData")}</Title>
+            </Card.Content>
+          </Card>
         );
-      });
+      }
     }
 
     return routeStopArrivalView;
   };
 
   const getMinutesDiffStr = (timestamp) => {
-    let minutesDiffStr = "";
+    let minutesDiffStr = t("noData");
 
-    const now = moment().tz("Asia/Hong_Kong");
-    const itemTime = moment(timestamp).tz("Asia/Hong_Kong");
+    if (timestamp) {
+      const now = moment().tz("Asia/Hong_Kong");
+      const itemTime = moment(timestamp).tz("Asia/Hong_Kong");
+      console.log("now = ", now);
+      console.log("itemTime = ", itemTime);
 
-    // console.log("now = ", now);
-    // console.log("itemTime = ", itemTime);
+      const minutesDiff = itemTime.diff(now, "minute");
+      console.log("minutesDiff = ", minutesDiff);
 
-    const minutesDiff = itemTime.diff(now, "minute");
-
-    // console.log("minutesDiff = ", minutesDiff);
-
-    if (minutesDiff > 0) {
-      minutesDiffStr = `${minutesDiff} minutes`;
-    } else {
-      minutesDiffStr = `Arriving`;
+      if (minutesDiff <= 1) {
+        minutesDiffStr = `Arriving`;
+      } else {
+        minutesDiffStr = `${minutesDiff} minutes`;
+      }
     }
 
     return minutesDiffStr;
